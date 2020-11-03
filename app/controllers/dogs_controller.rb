9238ba69f9f2224e6bb17579a1519651d67942f9
aarora16 <a.99.anshul@gmail.com>
2020-11-03 13:05:53 -0600

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,11 @@
 class DogsController < ApplicationController
+  def show
+    @dog = Dog.find(params[:id])
+    @employees = Employee.select {|employee| employee.dog_id == @dog.id}
+  end
+
+  def index
+    @dogs = Dog.all
+  end
+
 end
